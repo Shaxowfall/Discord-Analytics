412acba656d17ce14f395e101e3b96ec14b9c919
Shaxowfall <96609378+Shaxowfall@users.noreply.github.com>
2023-05-16 15:08:49 -0800

    Minor Typo Fix
    :)

--- Analytics-Kill.bat
+++ Analytics-Kill.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Starting Application Kill Process
 taskkill /f /im capture_helper.exe
-taskill /f /im gpu_encoder_helper.exe
-taskill /f /im DiscordHookHelper64.exe
-taskill /f /im DiscordHookHelper.exe
+taskkill /f /im gpu_encoder_helper.exe
+taskkill /f /im DiscordHookHelper64.exe
+taskkill /f /im DiscordHookHelper.exe
 echo Finished Killing Executables
 pause
